1fb22a01d70ebda2354742b1b30d0dc1390b2eec
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-06 11:26:24 +0100

    Creating an about page

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -1,15 +1,16 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 include __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 
 
 $app = new App();
 
-$app->get('/', [[HomeController::class], 'home']);
+$app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
