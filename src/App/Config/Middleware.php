5a0a9e424ce2a2ce195916d5b02fb340f115ed71
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-06 17:39:17 +0100

    Creating middleware

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Config;
 
+use App\Middleware\TemplateDataMiddleware;
 use Framework\App;
 
-function registerMiddleware(App $app) {}
+function registerMiddleware(App $app)
+{
+    $app->addMiddleware(TemplateDataMiddleware::class);
+}
