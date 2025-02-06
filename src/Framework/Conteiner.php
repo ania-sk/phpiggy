59ddb7f5db82dce53dce81162ed12e71ce922b98
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-06 13:17:31 +0100

    Reflective programming

--- src/Framework/Conteiner.php
+++ src/Framework/Conteiner.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use ReflectionClass;
+
 class Conteiner
 {
     private array $definitions = [];
 
     public function addDefinitions(array $newDefinitions)
     {
-        $this->definitions = [...$this->definitions, ...$newDefinitions];
-        dd($newDefinitions);
+        // $this->definitions = [...$this->definitions, ...$newDefinitions];
+        $this->definitions = array_merge($this->definitions, $newDefinitions);
+    }
+
+    public function resolve(string $className)
+    {
+        $reflectionClass = new ReflectionClass($className);
+
+        dd($reflectionClass);
     }
 }
