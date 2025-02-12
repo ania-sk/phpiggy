2b3c663e8504265a9051998a2632c8c714456f56
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-12 10:49:28 +0100

    Custom middleware

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Config;
 
-use App\Middleware\TemplateDataMiddleware;
+use App\Middleware\{TemplateDataMiddleware, ValidationExceptionMiddleware};
 use Framework\App;
 
 function registerMiddleware(App $app)
 {
     $app->addMiddleware(TemplateDataMiddleware::class);
+    $app->addMiddleware(ValidationExceptionMiddleware::class);
 }
