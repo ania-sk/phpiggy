2e97e3784c4b46615b6b157e92184151b55cad5d
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-12 10:58:05 +0100

    Redirection with headers

--- src/App/Middleware/ValidationExceptionMiddleware.php
+++ src/App/Middleware/ValidationExceptionMiddleware.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use Framework\Contracts\MiddlewareInterface;
+use Framework\Exceptions\ValidationException;
 
 class ValidationExceptionMiddleware implements MiddlewareInterface
 {
     public function process(callable $next)
     {
-        $next();
+        try {
+            $next();
+        } catch (ValidationException $e) {
+            redirectTo("/register");
+        }
     }
 }
