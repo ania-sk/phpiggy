c3a4a6600045b49457dd13cb9fd2775722d99c53
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-19 13:44:36 +0100

    Url validation rule

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use Framework\Rules\{RequiredRule, EmailRule, MinRule, InRule};
+use Framework\Rules\{RequiredRule, EmailRule, MinRule, InRule, UrlRule};
 use Framework\Validator;
 
 class ValidatorService
@@ -19,6 +19,7 @@ class ValidatorService
         $this->validator->add('email', new EmailRule());
         $this->validator->add('min', new MinRule());
         $this->validator->add('in', new InRule());
+        $this->validator->add('url', new UrlRule());
     }
 
     public function validateRegister(array $formData)
@@ -27,7 +28,7 @@ class ValidatorService
             'email' => ['required', 'email'],
             'age' => ['required', 'min:18'],
             'country' => ['required', 'in:USA,Canada,Mexico'],
-            'socialMediaURL' => ['required'],
+            'socialMediaURL' => ['required', 'url'],
             'password' => ['required'],
             'confirmPassword' => ['required'],
             'tos' => ['required']
