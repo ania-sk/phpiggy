cbc77506e7d24e231c1228aee7d8dee9b0d70e28
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-19 12:54:15 +0100

    Displaying errors

--- src/App/views/register.php
+++ src/App/views/register.php
@@ -1,7 +1,6 @@
 <?php include $this->resolve("partials/_header.php"); ?>
 <section
     class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
-    <?php var_dump($errors); ?>
     <form method="POST" class="grid grid-cols-1 gap-6">
         <!-- Email -->
         <label class="block">
@@ -11,6 +10,11 @@
                 type="email"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="john@example.com" />
+            <?php if (array_key_exists('email', $errors)): ?>
+                <div class="bg-gray-100 mt-2 text-red-500">
+                    <?php echo e($errors['email'][0]) ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Age -->
         <label class="block">
@@ -20,6 +24,11 @@
                 type="number"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+            <?php if (array_key_exists('age', $errors)): ?>
+                <div class="bg-gray-100 mt-2 text-red-500">
+                    <?php echo e($errors['age'][0]) ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Country -->
         <label class="block">
@@ -31,6 +40,11 @@
                 <option value="Mexico">Mexico</option>
                 <option value="Invalid">Invalid Country</option>
             </select>
+            <?php if (array_key_exists('country', $errors)): ?>
+                <div class="bg-gray-100 mt-2 text-red-500">
+                    <?php echo e($errors['country'][0]) ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Social Media URL -->
         <label class="block">
@@ -40,6 +54,11 @@
                 type="text"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+            <?php if (array_key_exists('socialMediaURL', $errors)): ?>
+                <div class="bg-gray-100 mt-2 text-red-500">
+                    <?php echo e($errors['socialMediaURL'][0]) ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Password -->
         <label class="block">
@@ -49,6 +68,11 @@
                 type="password"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+            <?php if (array_key_exists('password', $errors)): ?>
+                <div class="bg-gray-100 mt-2 text-red-500">
+                    <?php echo e($errors['password'][0]) ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Confirm Password -->
         <label class="block">
@@ -58,6 +82,11 @@
                 type="password"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+            <?php if (array_key_exists('confirmPassword', $errors)): ?>
+                <div class="bg-gray-100 mt-2 text-red-500">
+                    <?php echo e($errors['confirmPassword'][0]) ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Terms of Service -->
         <div class="block">
@@ -70,6 +99,11 @@
                             type="checkbox" />
                         <span class="ml-2">I accept the terms of service.</span>
                     </label>
+                    <?php if (array_key_exists('tos', $errors)): ?>
+                        <div class="bg-gray-100 mt-2 text-red-500">
+                            <?php echo e($errors['tos'][0]) ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
