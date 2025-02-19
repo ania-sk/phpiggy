135eae96a3bdd41665872e3948c5565be6a05369
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-19 13:05:14 +0100

    Validating emails

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use Framework\Rules\{RequiredRule};
+use Framework\Rules\{RequiredRule, EmailRule};
 use Framework\Validator;
 
 class ValidatorService
@@ -16,12 +16,13 @@ class ValidatorService
         $this->validator = new Validator();
 
         $this->validator->add('required', new RequiredRule());
+        $this->validator->add('email', new EmailRule());
     }
 
     public function validateRegister(array $formData)
     {
         $this->validator->validate($formData, [
-            'email' => ['required'],
+            'email' => ['required', 'email'],
             'age' => ['required'],
             'country' => ['required'],
             'socialMediaURL' => ['required'],
