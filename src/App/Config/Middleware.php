b08a222c646ea832bd3b6408ab6a3cd937bb55cf
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-02-19 11:48:09 +0100

    Enabling sessions

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace App\Config;
 
-use App\Middleware\{TemplateDataMiddleware, ValidationExceptionMiddleware};
+use App\Middleware\{
+    TemplateDataMiddleware,
+    ValidationExceptionMiddleware,
+    SessionMiddleware
+};
 use Framework\App;
 
 function registerMiddleware(App $app)
 {
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(SessionMiddleware::class);
 }
