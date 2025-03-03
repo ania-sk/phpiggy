12638eb9ce8649542fd53129565a41287b1fa4f3
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-03-03 13:00:26 +0100

    Creating a DSN string

--- cli.php
+++ cli.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hello world!";
+$driver = 'mysql';
+
+$config = http_build_query(data: [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'phpiggy'
+], arg_separator: ';');
+
+$dsn = "{$driver}:{$config}";
+
+echo $dsn;
