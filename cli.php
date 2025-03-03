d802de1a11ad3d0d6933914afbf86a410c6e8f29
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-03-03 13:32:52 +0100

    Refactoring the database connection

--- cli.php
+++ cli.php
@@ -1,21 +1,13 @@
 <?php
 
-$driver = 'mysql';
+include __DIR__ . "/src/Framework/Database.php";
 
-$config = http_build_query(data: [
+use Framework\Database;
+
+$db = new Database('mysql', [
     'host' => 'localhost',
     'port' => 3306,
-    'dbname' => 'phpiggy'
-], arg_separator: ';');
+    'dbname' => 'phpiggy',
+], 'root', '');
 
-$dsn = "{$driver}:{$config}";
-
-$username = 'root';
-$password = '';
-
-try {
-    $db = new PDO($dsn, $username, $password);
-} catch (PDOException $e) {
-    die("Unable to connect to database");
-}
 echo "Connected to database";
