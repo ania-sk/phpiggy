3b8cf946778a636c78cdb54a3cd3d4dd75ad419a
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-03-17 12:35:37 +0100

    Authenticating registered users

--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -40,6 +40,10 @@ class UserService
                 'url' => $formData['socialMediaURL']
             ]
         );
+
+        session_regenerate_id();
+
+        $_SESSION['user'] = $this->db->id();
     }
 
     public function login(array $formData)
