5d4b5bfa6c59f4721d70ab4903bd4d599e5d6e2b
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-03-18 14:33:42 +0100

    Limiting results

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -26,14 +26,15 @@ class TransactionService
         );
     }
 
-    public function getUserTransactions()
+    public function getUserTransactions(int $length, int $offset)
     {
         $searchTerm = addcslashes($_GET['s'] ?? '', '%_');
 
         $transactions = $this->db->query(
             "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date
             FROM transactions WHERE user_id = :user_id
-            AND description	LIKE :description",
+            AND description	LIKE :description
+            LIMIT {$length} OFFSET {$offset}",
             [
                 'user_id' => $_SESSION['user'],
                 'description' => "%{$searchTerm}%"
