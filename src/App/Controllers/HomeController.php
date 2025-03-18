c09c98d0aad981499aa3fa7a75387c4e00b6a7bc
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-03-18 14:55:10 +0100

    Next page link

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -22,16 +22,23 @@ class HomeController
         $offset = ($page - 1) * $length;
         $searchTerm = $_GET['s'] ?? null;
 
-        $transactions = $this->transactionService->getUserTransactions(
+        [$transactions, $count] = $this->transactionService->getUserTransactions(
             $length,
             $offset
         );
+
+        $lastPage = ceil($count / $length);
 
         echo $this->view->render("index.php", [
             'transactions' => $transactions,
             'currentPage' => $page,
             'previousPageQuery' => http_build_query([
                 'p' => $page - 1,
+                's' => $searchTerm
+            ]),
+            'lastPage' => $lastPage,
+            'nextPageQuery' => http_build_query([
+                'p' => $page + 1,
                 's' => $searchTerm
             ])
         ]);
