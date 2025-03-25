4e80499022266246958500ba1dc76f8ac62a89a3
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-03-25 12:39:57 +0100

    Overriding HTTP methods

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -36,7 +36,7 @@ class Router
     public function dispatch(string $path, string $method, Conteiner $container = null)
     {
         $path = $this->normalizePath($path);
-        $method = strtoupper($method);
+        $method = strtoupper($_POST['_METHOD'] ?? $method);
 
         foreach ($this->routes as $route) {
             if (
